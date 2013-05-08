e67eb96c0f338e1fc785a44abd3ecdb33a245d6d
Saurabh Mehta <saurabhmehta123@gmail.com>
2013-05-08 15:17:51 +0530

    Renamed migration files for categorie

---
+++ application/migrations/2013_05_08_073810_alter_category_product_table.php
@@ -0,0 +1,33 @@
+<?php
+
+class Alter_Categorie_Product_Table {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('category_product', function($table)
+			{
+				$table->foreign('product_id')->references('id')->on('products');			
+				$table->foreign('category_id')->references('id')->on('categories');			
+			});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('category_product', function($table)
+		{
+			$table->drop_foreign('category_product_product_id_foreign');
+			$table->drop_foreign('category_product_category_id_foreign');
+		});
+	}
+
+}

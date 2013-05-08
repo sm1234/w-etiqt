d08f7d599888e87d94b9a986ba8e5284d6475964
Saurabh Mehta <saurabhmehta123@gmail.com>
2013-05-08 15:37:38 +0530

    Removed create_tags migration script and added again with right name

---
+++ application/migrations/2013_05_08_075915_create_tags_table.php
@@ -0,0 +1,28 @@
+<?php
+
+class Create_Tags {
+
+	/*Create the tags table*/
+	public function up()
+	{
+		Schema::table('tags', function($table)
+		{
+			$table->create();
+			$table->increments('id');
+			$table->string('description',100);
+			$table->timestamps();
+		});
+	}
+
+	/*
+	 * 
+	 *Drop the tags table
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tags');
+	}
+
+}
+?>
---
+++ application/migrations/2013_05_08_095734_create_product_tag_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Laravel\Database\Schema;
+
+class Create_Product_Tag_Table {
+
+	/**
+	 * Created product_tag table
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('product_tag', function($table){
+			$table->create();
+			$table->increments('id');
+			$table->integer('product_id')->unsigned();
+			$table->integer('tag_id')->unsigned();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Drops product_tag table
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('product_tag');
+	}
+
+}
